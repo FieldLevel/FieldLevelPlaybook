2e636c7134598497b587c09205fad9f9cd520e74
Scott Mathson <smathson@fieldlevel.com>
2021-02-09 12:39:23 -0800

    Update Button and add docs

--- docs/Actions/Button.stories.tsx
+++ docs/Actions/Button.stories.tsx
@@ -1,6 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 
-import { Button } from '../../src';
+import { Button, ButtonGroup, AddMinor } from '../../src';
 
 export const Default = (args: any) => <Button {...args}>Action</Button>;
+
+export const Basic = () => <Button>Continue</Button>;
+
+export const Primary = () => <Button variant="primary">Upgrade</Button>;
+
+export const Plain = () => <Button variant="plain">Learn More</Button>;
+
+export const Destructive = () => <Button variant="destructive">Delete</Button>;
+
+export const WithIcon = () => <Button icon={AddMinor}>Add Athlete</Button>;
+
+export const Size = () => (
+    <ButtonGroup>
+        <Button size="slim">Slim</Button>
+        <Button>Default</Button>
+        <Button size="large">Large</Button>
+    </ButtonGroup>
+);
+
+export const Disabled = () => (
+    <ButtonGroup>
+        <Button disabled>Basic</Button>
+        <Button variant="primary" disabled>
+            Primary
+        </Button>
+        <Button variant="plain" disabled>
+            Plain
+        </Button>
+    </ButtonGroup>
+);
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,11 +7,12 @@ import styles from './Button.module.css';
 
 type size = 'slim' | 'large';
 
-type variant = 'basic' | 'primary' | 'plain' | 'destructive';
+type variant = 'primary' | 'plain' | 'destructive';
 
 export interface ButtonProps {
     size?: size;
     variant?: variant;
+    disabled?: boolean;
     icon?: React.FC<React.SVGProps<SVGSVGElement>>;
     children?: string | React.ReactElement;
     onClick?(): void;
@@ -23,14 +24,18 @@ const sizeStyles: { [key in size]: string } = {
 };
 
 const variantStyles: { [key in variant]: string } = {
-    basic: styles.basic,
     primary: styles.primary,
     destructive: styles.destructive,
     plain: styles.plain
 };
 
-export const Button = ({ size, variant = 'basic', icon, onClick, children }: ButtonProps) => {
-    const className = cx(styles.Button, size && sizeStyles[size], variant && variantStyles[variant]);
+export const Button = ({ size, variant, disabled, icon, onClick, children }: ButtonProps) => {
+    const className = cx(
+        styles.Button,
+        size && sizeStyles[size],
+        variant && variantStyles[variant],
+        disabled && styles.disabled
+    );
     const iconContent = icon && (
         <span className={styles.Icon}>
             <Icon source={icon} color="current" />
@@ -38,7 +43,7 @@ export const Button = ({ size, variant = 'basic', icon, onClick, children }: But
     );
 
     return (
-        <button className={className} onClick={onClick}>
+        <button className={className} disabled={disabled} onClick={onClick}>
             {iconContent}
             {children}
         </button>
