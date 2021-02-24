c35203826e2400ab0ea0c73c7e8ac1b5af72fd99
Scott Mathson <smathson@fieldlevel.com>
2021-02-24 13:27:35 -0800

    Add dev badge to Feedback

---
+++ docs/utils.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const DevBadge = () => (
+    <div className="inline-block mt-2 px-2 py-1 rounded font-bold text-critical bg-background-critical">
+        In Development
+    </div>
+);
