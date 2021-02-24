e51ac23e6b6361b30573ab4e89bb82330caa6c48
Scott Mathson <smathson@fieldlevel.com>
2021-02-24 13:28:03 -0800

    Create Status doc for initial release

--- docs/utils.tsx
+++ docs/utils.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import cx from 'classnames';
 
-export const DevBadge = () => (
-    <div className="inline-block mt-2 px-2 py-1 rounded font-bold text-critical bg-background-critical">
-        In Development
-    </div>
-);
+const colorStyles = {
+    success: 'text-success bg-background-success',
+    warning: 'text-warning bg-background-warning',
+    critical: 'text-critical bg-background-critical',
+    muted: 'text-muted bg-background-muted'
+};
+
+const Badge = ({ type, children }) => {
+    const colorStyle = colorStyles[type];
+    const style = cx('inline-block mt-2 px-2 py-0.5 rounded shadow', colorStyle);
+    return <div className={style}>{children}</div>;
+};
+
+export const InDevelopment = () => <Badge type="warning">In Development</Badge>;
+
+export const Released = ({ version }) => <Badge type="success">Released in v{version}</Badge>;
+
+export const Deprecated = ({ version }) => <Badge type="critical">Deprecated in v{version}</Badge>;
+
+export const Planned = () => <Badge type="muted">Planned</Badge>;
