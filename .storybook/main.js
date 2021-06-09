75b2ff671a5a69a4b06cac8de58a2f93517fcd45
Scott Mathson <smathson@fieldlevel.com>
2021-06-09 13:25:28 -0700

    Upgrade Storybook and stop using PostCSS 7 tailwind package

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,29 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     stories: ['../bullpen/index.ts', '../docs/**/*.stories.mdx'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss')
+                }
+            }
+        }
+    ],
+    // workaround for babel issue supposedly fixed in storybook 6.3: https://github.com/storybookjs/storybook/issues/14805
+    babel: async (options) => {
+        const { plugins = [] } = options;
+        return {
+            ...options,
+            plugins: [
+                ...plugins,
+                [require.resolve('@babel/plugin-proposal-private-property-in-object'), { loose: true }]
+            ]
+        };
+    },
     webpackFinal: (config) => {
         const cssModulesRule = {
             test: /\.module\.css$/,
@@ -46,10 +68,12 @@ module.exports = {
         const extraRules = [cssModulesRule, globalCssRule];
 
         // remove the existing css loader rule
-        config.module.rules = config.module.rules.filter((rule) => !rule.test.test('.css'));
+        config.module.rules = config.module.rules.filter((rule) => rule.test && !rule.test.test('.css'));
 
         // disable file loader rule for SVGs
-        const fileLoaderRule = config.module.rules.find((rule) => !Array.isArray(rule.test) && rule.test.test('.svg'));
+        const fileLoaderRule = config.module.rules.find(
+            (rule) => rule.test && !Array.isArray(rule.test) && rule.test.test('.svg')
+        );
         fileLoaderRule.exclude = /\.svg$/;
 
         // add additional rules
