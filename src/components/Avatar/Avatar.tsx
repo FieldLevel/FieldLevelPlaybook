f18129d72d3edfeb28db869489cdfc4e646c826b
Attila <attilabuturla@gmail.com>
2021-10-26 11:24:37 -0700

    Update Avatar Component to use new LazyImage component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import cx from 'classnames';
+import { LazyImage } from '../LazyImage';
 
 import styles from './Avatar.module.css';
 
 type size = 'small' | 'large';
+type pixelSize = size | 'base';
 
 export interface AvatarProps {
     source?: string;
     size?: size;
     alt?: string;
+    lazy?: boolean;
 }
 
 const sizeStyles: { [key in size]: string } = {
@@ -16,12 +19,19 @@ const sizeStyles: { [key in size]: string } = {
     large: styles.large
 };
 
-export const Avatar = ({ source, size, alt }: AvatarProps) => {
+const sizeInPixels: { [key in pixelSize]: number } = {
+    small: 20,
+    base: 40,
+    large: 60
+};
+
+export const Avatar = ({ source, size, alt, lazy }: AvatarProps) => {
     const avatarStyle = cx(styles.Avatar, size && sizeStyles[size]);
+    const dimension = sizeInPixels[size || 'base'] || sizeInPixels.base;
 
     return (
         <span role="img" className={avatarStyle}>
-            <img src={source} className={styles.Image} alt={alt} role="presentation" />
+            <LazyImage src={source} height={dimension} width={dimension} alt={alt} lazy={lazy} />
         </span>
     );
 };
