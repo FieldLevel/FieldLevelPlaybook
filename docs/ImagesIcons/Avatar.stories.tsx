3d8e4bae5c2a4e7db19b31e67db2b7ef610d0527
Attila <attilabuturla@gmail.com>
2021-10-27 14:38:51 -0700

    Add story for Avatar lazy

--- docs/ImagesIcons/Avatar.stories.tsx
+++ docs/ImagesIcons/Avatar.stories.tsx
@@ -12,3 +12,22 @@ export const Size = () => (
         <Avatar size="large" source="http://placekitten.com/120/120" />
     </Stack>
 );
+
+export const Lazy = () => {
+    const images = 16;
+    const urls: string[] = [];
+
+    for (let i = 1; i <= images; i++) {
+        urls.push(`http://placekitten.com/80/80?image=${i}`);
+    }
+
+    return (
+        <Stack vertical>
+            {urls.map((url, index) => (
+                <div key={index} className="pb-4">
+                    <Avatar lazy source={url} />
+                </div>
+            ))}
+        </Stack>
+    );
+};
