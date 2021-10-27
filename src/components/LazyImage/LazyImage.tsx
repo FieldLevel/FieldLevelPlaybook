01383b8dee5019358144ac332bb24d4c6bfd895b
Attila <attilabuturla@gmail.com>
2021-10-27 15:48:58 -0700

    Separate native lazy behavior from fallback behavior

--- src/components/LazyImage/LazyImage.tsx
+++ src/components/LazyImage/LazyImage.tsx
@@ -10,28 +10,47 @@ export interface LazyImageProps {
     lazy?: boolean;
 }
 
-export const LazyImage = ({ src, alt, width, height, lazy }: LazyImageProps) => {
+const NativeLazyImage = ({ src, alt, width, height, lazy }: LazyImageProps) => {
+    return (
+        <img
+            loading={lazy ? 'lazy' : 'eager'}
+            src={src}
+            width={width}
+            height={height}
+            alt={alt}
+            className={styles.Image}
+            role="presentation"
+        />
+    );
+};
+
+const FallbackLazyImage = ({ src, alt, width, height }: LazyImageProps) => {
     const ratioPadding = (height / width) * 100;
-    const supportsLazyLoading = HTMLImageElement.prototype.hasOwnProperty('loading');
     const { ref, inView } = useInView({
         rootMargin: '200px 0px',
         triggerOnce: true,
-        skip: supportsLazyLoading !== false
+        skip: false
     });
 
     return (
         <div ref={ref} style={{ position: 'relative', paddingBottom: `${ratioPadding}%` }}>
-            {inView || supportsLazyLoading ? (
-                <img
-                    loading={lazy ? 'lazy' : 'eager'}
-                    src={src}
-                    width={width}
-                    height={height}
-                    alt={alt}
-                    className={styles.Image}
-                    role="presentation"
-                />
+            {inView ? (
+                <img src={src} width={width} height={height} alt={alt} className={styles.Image} role="presentation" />
             ) : null}
         </div>
+    );
+};
+
+export const LazyImage = ({ src, alt, width, height, lazy }: LazyImageProps) => {
+    const supportsLazyLoading = HTMLImageElement.prototype.hasOwnProperty('loading');
+
+    return (
+        <>
+            {supportsLazyLoading && false ? (
+                <NativeLazyImage lazy={lazy} src={src} alt={alt} height={height} width={width} />
+            ) : (
+                <FallbackLazyImage src={src} alt={alt} height={height} width={width} />
+            )}
+        </>
     );
 };
