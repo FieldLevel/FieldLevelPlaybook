2a8bb693fabb7ae6d332bdb48477a478aa506b22
Scott Mathson <smathson@fieldlevel.com>
2021-11-01 10:58:50 -0700

    Replace lazy story with cover story for LazyImage

--- docs/ImagesIcons/LazyImage.stories.tsx
+++ docs/ImagesIcons/LazyImage.stories.tsx
@@ -15,23 +15,15 @@ Default.args = {
     height: 40
 };
 
-export const Lazy = () => {
-    const images = 16;
-    const urls: string[] = [];
-
-    for (let i = 1; i <= images; i++) {
-        urls.push(`http://placekitten.com/80/80?image=${i}`);
-    }
-
+export const Cover = () => {
     return (
-        <Stack vertical>
-            {urls.map((url, index) => (
-                <Stack.Item key={index}>
-                    <div className="h-10 w-10 pb-4">
-                        <LazyImage src={url} height={40} width={40} />
-                    </div>
-                </Stack.Item>
-            ))}
+        <Stack>
+            <div className="h-20 w-20">
+                <LazyImage src="http://placekitten.com/80/120" height={80} width={80} />
+            </div>
+            <div className="h-20 w-20">
+                <LazyImage src="http://placekitten.com/80/120" height={80} width={80} cover />
+            </div>
         </Stack>
     );
 };
