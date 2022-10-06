deec622b1e0bb7576bb5324d0d805fa5a6c324a0
Scott Mathson <smathson@fieldlevel.com>
2022-10-06 12:23:32 -0700

    Add unstyled option to to Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import cx from 'classnames';
+
+import styles from './Link.module.css';
 
 export interface LinkProps {
     url?: string;
     external?: boolean;
     target?: string;
+    unstyled?: boolean;
     children?: React.ReactNode;
     onClick?(e?: React.MouseEvent<HTMLAnchorElement>): void;
 }
 
-export const Link = ({ url, target, external, children, onClick }: LinkProps) => {
+export const Link = ({ url, target, external, unstyled, children, onClick }: LinkProps) => {
     const targetValue = target || (external ? '_blank' : '');
     const rel = targetValue == '_blank' ? 'noopener noreferrer' : '';
+    const style = cx(unstyled && styles.unstyled);
 
     return (
-        <a href={url} target={targetValue} rel={rel} onClick={onClick}>
+        <a href={url} target={targetValue} rel={rel} className={style} onClick={onClick}>
             {children}
         </a>
     );
