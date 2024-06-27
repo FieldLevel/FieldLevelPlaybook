497ba0483f88c9505cd057c68f18fc5fb8a47fd0
RZ <rziegler@fieldlevel.com>
2024-06-27 09:39:01 -0400

    Chore: add ForwardRef to Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ForwardedRef } from 'react';
 import cx from 'classnames';
 
 import styles from './Link.module.css';
@@ -16,18 +16,25 @@ export interface LinkProps {
     onClick?(e?: React.MouseEvent<HTMLAnchorElement>): void;
 }
 
-export const Link = ({ url, target, external, unstyled, fullWidth, children, onClick }: LinkProps) => {
-    const targetValue = target || (external ? '_blank' : '');
-    const rel = targetValue == '_blank' ? 'noopener noreferrer' : '';
-    const style = cx(unstyled && styles.unstyled, fullWidth && styles.fullWidth);
+export const Link = React.forwardRef(
+    (
+        { url, target, external, unstyled, fullWidth, children, onClick }: LinkProps,
+        ref: ForwardedRef<HTMLAnchorElement>
+    ) => {
+        const targetValue = target || (external ? '_blank' : '');
+        const rel = targetValue == '_blank' ? 'noopener noreferrer' : '';
+        const style = cx(unstyled && styles.unstyled, fullWidth && styles.fullWidth);
 
-    if (external) {
-        console.warn(`Link :: The external prop has been deprecated. Use target instead.`);
+        if (external) {
+            console.warn(`Link :: The external prop has been deprecated. Use target instead.`);
+        }
+
+        return (
+            <a href={url} target={targetValue} rel={rel} className={style} onClick={onClick} ref={ref}>
+                {children}
+            </a>
+        );
     }
+);
 
-    return (
-        <a href={url} target={targetValue} rel={rel} className={style} onClick={onClick}>
-            {children}
-        </a>
-    );
-};
+Link.displayName = 'Link';
