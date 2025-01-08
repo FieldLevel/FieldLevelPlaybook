6ce61452c1f465a7fd234f6dd5887f8e1bb4bd8e
RZ <rziegler@fieldlevel.com>
2025-01-08 15:19:49 -0500

    Chore: convert from yarn to npm

--- scripts/build.js
+++ scripts/build.js
@@ -6,19 +6,19 @@ const root = resolve(__dirname, '..');
 const run = (cmd) => execSync(cmd, { stdio: 'inherit', cwd: root });
 
 // Clean dist directory
-run(`yarn run rimraf dist`);
+run(`npx rimraf dist`);
 
 // Generate type definitions
-run(`yarn run tsc --emitDeclarationOnly --outDir dist/types`);
+run(`npx tsc --emitDeclarationOnly --outDir dist/types`);
 
 // Generate final JS and CSS
-run(`yarn run rollup -c --environment NODE_ENV:production`);
+run(`npx rollup -c --environment NODE_ENV:production`);
 
 // Copy tailwind preset
-run(`yarn run cpy tailwind.preset.js dist`);
+run(`npx cpy tailwind.preset.js dist`);
 
 // Copy prettier config
-run(`yarn run cpy prettier.config.js dist`);
+run(`npx cpy prettier.config.js dist`);
 
 // Copy browserslist config
-run(`yarn run cpy .browserslistrc dist`);
+run(`npx cpy .browserslistrc dist`);
--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -6,10 +6,10 @@ const root = resolve(__dirname, '..');
 const run = (cmd) => execSync(cmd, { stdio: 'inherit', cwd: root });
 
 // Clean output directory
-run(`yarn run rimraf src/icons`);
+run(`npx rimraf src/icons`);
 
 // Run SVGR to convert raw icon SVGs to react components
-run(`yarn run svgr --config-file svgr.config.js -d src/icons icons`);
+run(`npx svgr --config-file svgr.config.js -d src/icons icons`);
 
 // Run prettier on automatically generated index.tsx
-run(`yarn run prettier --write src/icons/**/index.tsx`);
+run(`npx prettier --write src/icons/**/index.tsx`);
